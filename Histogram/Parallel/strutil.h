a38cd3b0df13bad0ab9050d4e7063776f8855fc7
Ian Dunn <iondune@gmail.com>
2013-10-26 21:11:24 -0700

    + Compiling version of MrMain

---
+++ Histogram/Parallel/strutil.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INT_STRING_LEN   11 // "-4000000000"
+#define MAX_FLOAT_STRING_LEN 20 // Guesstimated
+
+/**
+ * Splits a string of elements into chunks of elements, and performs an action
+ * on each chunk.
+ *
+ * @param data The string of elements.
+ * @param delim The delimeter.
+ * @param extra Extra data to pass to the callback.
+ * @param f The callback method to process each chunk. Its parameters are:
+ *            char*      The chunk of data.
+ *            int        The ordinal value of this chunk (0, 1, ...)
+ *            const char The delimiter passed into chunks.
+ *            int        The number of elements in the chunk (delimited by |delim|).
+ *            void*      The extra data passed into chunks.
+ */
+void withChunks(char* data, 
+                const char delim, 
+                int chunkSize,
+                void* extra, 
+                void (*f)(char*, int, const char, int, int, void*)); 
+/**
+ * Like withChunks, but uses ' ' as a delimiter.
+ */
+void withChunksSpace(char* data, 
+                     int chunkSize,
+                     void* extra, 
+                     void (*f)(char*, int, const char, int, int, void*));
+
+inline char* firstToken(char* data) {
+  return strtok(data, " ");
+}
+
+inline char* nextToken() {
+  return strtok(NULL, " ");
+}
