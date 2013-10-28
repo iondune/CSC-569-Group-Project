caa44cfc8208162ccfd097d401f99f1b18a7871a
Ian Dunn <iondune@gmail.com>
2013-10-27 18:22:32 -0700

    + (FT) More folder directory changes

---
+++ Histogram/FaultTolerant/Application/NetworkBenchmarkMasterApplication.h
@@ -0,0 +1,85 @@
+
+#pragma once
+
+#include "Application.h"
+#include <mpi.h>
+#include "MappedFile.h"
+
+
+class NetworkBenchmarkMasterApplication : public Application
+{
+
+    std::string FileNameA, FileNameB;
+
+public:
+
+    NetworkBenchmarkMasterApplication(std::string const & fileNameA, std::string const & fileNameB)
+    {
+        FileNameA = fileNameA;
+        FileNameB = fileNameB;
+        Profiler.SetProcessorId(0);
+    }
+
+    void Run()
+    {
+        ReadInFiles();
+        SendVectorsToSlaves();
+        CalculateSum();
+        MakeHistograms();
+        WriteOutputFiles();
+    }
+
+    void ReadInFiles()
+    {
+        Profiler.Start("Map");
+        MappedFile FileA(FileNameA), FileB(FileNameB);
+        Profiler.End();
+        
+        Profiler.Start("Read");
+        A.ParseFromString(FileA.Contents);
+        B.ParseFromString(FileB.Contents);
+        VectorSize = A.Values.size();
+        Profiler.End();
+    }
+
+    void SendVectorsToSlaves()
+    {
+        Profiler.Start("Send");
+        MPI_Bcast(& VectorSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& A.Values.front(), VectorSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& B.Values.front(), VectorSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& A.Maximum, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& B.Maximum, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        Profiler.End();
+    }
+
+    void CalculateSum()
+    {
+        Profiler.Start("Sum");
+        C.MakeSum(A, B);
+        C.Maximum = A.Maximum + B.Maximum;
+        Profiler.End();
+    }
+
+    void MakeHistograms()
+    {
+        Profiler.Start("Hist");
+        static float const BinWidth = 0.5f;
+        static float const Min = -10.f;
+        HistA = A.MakeHistogram(Min, BinWidth);
+        HistB = B.MakeHistogram(Min, BinWidth);
+        HistC = C.MakeHistogram(Min*2, BinWidth);
+        Profiler.End();
+    }
+
+    void WriteOutputFiles()
+    {
+        Profiler.Start("Write");
+        C.WriteToFile("result.out");
+        A.WriteHistogramToFile(HistA, "hist.a");
+        B.WriteHistogramToFile(HistB, "hist.b");
+        C.WriteHistogramToFile(HistC, "hist.c");
+        Profiler.End();
+    }
+
+};
---
+++ Histogram/FaultTolerant/Application/NetworkBenchmarkSlaveApplication.h
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include "Application.h"
+
+
+class NetworkBenchmarkSlaveApplication : public Application
+{
+
+public:
+
+    NetworkBenchmarkSlaveApplication(int ProcessorId)
+    {
+        Profiler.SetProcessorId(ProcessorId);
+    }
+
+    void Run()
+    {
+        ReceiveVectorsFromMaster();
+    }
+
+    void ReceiveVectorsFromMaster()
+    {
+        Profiler.Start("Recv");
+        MPI_Bcast(& VectorSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        A.Values.resize(VectorSize);
+        B.Values.resize(VectorSize);
+        MPI_Bcast(& A.Values.front(), VectorSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& B.Values.front(), VectorSize, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& A.Maximum, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(& B.Maximum, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+        Profiler.End();
+    }
+
+};
