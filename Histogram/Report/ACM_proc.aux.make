f7b3cbb0b95ae1c6d4841ce90864c90a4d3daab3
Austin Wylie <awwylie@gmail.com>
2013-10-29 00:41:42 -0700

    Started report. Layout set up with sections, tables, and figures; contains intro and discussion on non-fault-tolerant program design.

---
+++ Histogram/Report/ACM_proc.aux.make
@@ -0,0 +1,20 @@
+\@writefile{lof}{\contentsline {figure}{\numberline {1}{\ignorespaces Histogram for 10-Count Vectors}}{\thepage }}
+\@writefile{lof}{\contentsline {figure}{\numberline {2}{\ignorespaces Histogram for 10,000-Count Vectors}}{\thepage }}
+\@writefile{lof}{\contentsline {figure}{\numberline {3}{\ignorespaces Histogram for 100,000-Count Vectors}}{\thepage }}
+\@writefile{lof}{\contentsline {figure}{\numberline {4}{\ignorespaces Histogram for 1,000,000-Count Vectors}}{\thepage }}
+\@writefile{lof}{\contentsline {figure}{\numberline {5}{\ignorespaces Histogram for 10,000,000-Count Vectors}}{\thepage }}
+\@writefile{lot}{\contentsline {table}{\numberline {1}{\ignorespaces Histogram Creation Performance}}{\thepage }}
+\@writefile{toc}{\contentsline {section}{\numberline {1}Introduction}{\thepage }}
+\@writefile{toc}{\contentsline {section}{\numberline {2}Part A: Distributed Performance}{\thepage }}
+\@writefile{toc}{\contentsline {section}{\numberline {3}Part B: Fault-Tolerance}{\thepage }}
+\@writefile{toc}{\contentsline {subsection}{\numberline {2.1}Program Setup}{\thepage }}
+\@writefile{toc}{\contentsline {subsection}{\numberline {2.2}Performance Analysis}{\thepage }}
+\@writefile{toc}{\contentsline {subsection}{\numberline {3.1}Program Setup}{\thepage }}
+\@writefile{toc}{\contentsline {subsection}{\numberline {3.2}Performance Analysis}{\thepage }}
+\bibstyle{abbrv}
+\newlabel{PiTable}
+\newlabel{hist10000000}
+\newlabel{hist1000000}
+\newlabel{hist100000}
+\newlabel{hist10000}
+\newlabel{hist10}
---
+++ Histogram/Report/ACM_proc.d
@@ -0,0 +1,46 @@
+# vim: ft=make
+.PHONY: ACM_proc._graphics
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/etexcmds.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/ifluatex.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/ifpdf.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/infwarerr.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/kvdefinekeys.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/kvsetkeys.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/ltxcmds.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/pdftexcmds.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsfonts/amsfonts.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsfonts/amssymb.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsbsy.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsgen.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsmath.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amsopn.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/amsmath/amstext.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/fontenc.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/latexsym.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/float/float.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/epsfig.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/graphics.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/graphicx.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/keyval.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/graphics/trig.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/epstopdf-base.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/grfext.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/kvoptions.sty)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,ACM_proc.tex)
+ACM_proc.aux ACM_proc.aux.make ACM_proc.d ACM_proc.pdf: $(call path-norm,acm_proc_article-sp.cls)
+.SECONDEXPANSION:
+-include Pics/BT.png.gpi.d
+ACM_proc.d: $$(call graphics-source,Pics/BT.png)
+ACM_proc.pdf ACM_proc._graphics: $$(call graphics-target,Pics/BT.png)
+-include Pics/BT.png.gpi.d
+ACM_proc.d: $$(call graphics-source,Pics/BT.png)
+ACM_proc.pdf ACM_proc._graphics: $$(call graphics-target,Pics/BT.png)
+-include Pics/BT.png.gpi.d
+ACM_proc.d: $$(call graphics-source,Pics/BT.png)
+ACM_proc.pdf ACM_proc._graphics: $$(call graphics-target,Pics/BT.png)
+-include Pics/BT.png.gpi.d
+ACM_proc.d: $$(call graphics-source,Pics/BT.png)
+ACM_proc.pdf ACM_proc._graphics: $$(call graphics-target,Pics/BT.png)
+-include Pics/BT.png.gpi.d
+ACM_proc.d: $$(call graphics-source,Pics/BT.png)
+ACM_proc.pdf ACM_proc._graphics: $$(call graphics-target,Pics/BT.png)
